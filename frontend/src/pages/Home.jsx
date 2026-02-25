f505318ef69762d04921484a09960000c829690b
Monsoon Dibragede <monsoon.dibragede@gmail.com>
2026-02-25 11:50:50 +0530

    updated .env file

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,7 +7,7 @@ import ScenesCard from '../components/ScenesCard';
 import ErrorAlert from '../components/ErrorAlert';
 import LoadingSpinner from '../components/LoadingSpinner';
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL || 'https://livya.onrender.com';
 
 
 export default function Home() {
