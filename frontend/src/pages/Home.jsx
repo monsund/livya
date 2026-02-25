0469efdb693ed227262db91a633c7fe5a19a80a9
Monsoon Dibragede <monsoon.dibragede@gmail.com>
2026-02-25 11:54:54 +0530

    demo commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,6 +7,7 @@ import ScenesCard from '../components/ScenesCard';
 import ErrorAlert from '../components/ErrorAlert';
 import LoadingSpinner from '../components/LoadingSpinner';
 
+// const API_URL = import.meta.env.VITE_API_URL || 'https://livya.onrender.com';
 const API_URL = import.meta.env.VITE_API_URL || 'https://livya.onrender.com';
 
 
